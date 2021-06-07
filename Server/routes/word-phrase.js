f9f75ce749549f220e885b15913d313277b1bbb6
chitaranjan <chitabiswal4@gmail.com>
2021-06-07 10:58:42 +0530

    updated

--- Server/routes/word-phrase.js
+++ Server/routes/word-phrase.js
@@ -1,11 +1,9 @@
-const Express = require('express');
+const Express = require("express");
 const router = Express.Router();
-const ObjectId = require('mongodb').ObjectID;
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const WordPhrase = require('../model/WordPhrase');
+const ObjectId = require("mongodb").ObjectID;
+const WordPhrase = require("../model/WordPhrase");
 
-router.post('/post-wordphrase', async (req, res) => {
+router.post("/post-wordphrase", async (req, res) => {
   const data = req.body;
   const Wordphrase = new WordPhrase({
     wp: data.wp,
@@ -13,54 +11,54 @@ router.post('/post-wordphrase', async (req, res) => {
   });
   Wordphrase.save()
     .then((response) => {
-      res.send({data: response});
+      res.send({ data: response });
     })
     .catch((err) => {
       console.log(err);
     });
 });
 
-router.get('/get-wordphrase', async (req, res) => {
+router.get("/get-wordphrase", async (req, res) => {
   WordPhrase.find().then((data) => {
     res.send(data);
   });
 });
 
-router.delete('/wordphrase/delete', async (req, res) => {
+router.delete("/wordphrase/delete", async (req, res) => {
   const data = req.body;
   var id = ObjectId(data.id);
   var check = await WordPhrase.find({
-    $and: [{owner: data.owner}, {_id: id}],
+    $and: [{ owner: data.owner }, { _id: id }],
   });
   if (check.length > 0) {
-    WordPhrase.findByIdAndDelete({_id: id}, function (err, response) {
+    WordPhrase.findByIdAndDelete({ _id: id }, function (err, response) {
       if (err) {
         console.log(err);
       } else {
-        res.send({msg: 'delete successfully'});
+        res.send({ msg: "delete successfully" });
       }
     });
   } else {
-    return res.send({err: 'you are not the owner to delete this phrase'});
+    return res.send({ err: "you are not the owner to delete this phrase" });
   }
 });
 
-router.put('/wordphrase/update', async (req, res) => {
+router.put("/wordphrase/update", async (req, res) => {
   const data = req.body;
   var id = ObjectId(data.id);
   var check = await WordPhrase.find({
-    $and: [{owner: data.owner}, {_id: id}],
+    $and: [{ owner: data.owner }, { _id: id }],
   });
   if (check.length > 0) {
-    WordPhrase.findByIdAndUpdate(id, {wp: data.data}, {new: true})
+    WordPhrase.findByIdAndUpdate(id, { wp: data.data }, { new: true })
       .then((response) => {
-        res.json({data: response});
+        res.json({ data: response });
       })
       .catch((err) => {
-        res.send({err: 'Unable to update '});
+        res.send({ err: "Unable to update " });
       });
   } else {
-    return res.send({err: 'Only owner can edit'});
+    return res.send({ err: "Only owner can edit" });
   }
 });
 
